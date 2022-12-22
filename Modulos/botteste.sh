35b586bea38c2c4a76d0a23175d6014aae2060fd
TelksBr <vinizika508@gmail.com>
2022-12-22 13:15:48 -0300

    1.8

--- Modulos/botteste.sh
+++ Modulos/botteste.sh
@@ -10,7 +10,7 @@
     echo -e "\033[1;32mINICIANDO BOT TESTE \033[0m\n"
     cd $HOME/BOT
     rm -rf $HOME/BOT/botssh
-    wget https://github.com/ScottBrabo/PR0/raw/main/Sistema/botssh >/dev/null 2>&1
+    wget https://github.com/TelksBr/SSH_T_PROJECT_PLUS/raw/main/Sistema/botssh >/dev/null 2>&1
     chmod 777 botssh
     echo ""
     sleep 1
